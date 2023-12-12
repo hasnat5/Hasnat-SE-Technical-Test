b24773363536429ba053b9a5edeeff7184a2157e
hasnat5 <hasnatferdiananda5@gmail.com>
2023-12-12 23:52:09 +0700

    first commit

---
+++ test-3/src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from 'next'
+import { Plus_Jakarta_Sans } from 'next/font/google'
+import './globals.css'
+import { Providers } from './providers'
+
+// const inter = Inter({ subsets: ['latin'] })
+const plusJakartaSans = Plus_Jakarta_Sans({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'GITS.ID',
+  description: 'Landing page clone gits.id by hasnat',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={plusJakartaSans.className}>
+        <Providers>
+          {children}
+        </Providers>
+      </body>
+    </html>
+  )
+}
